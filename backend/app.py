3591a5cf4a944bff7a705f357af89818caef2451
TestDevRp <pradhan.rojalin@gmail.com>
2025-02-22 13:33:04 +0000

    app.py

---
+++ backend/app.py
@@ -0,0 +1,33 @@
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://user:password@db/tasks_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    completed = db.Column(db.Boolean, default=False)
+
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    tasks = Task.query.all()
+    return jsonify([{'id': t.id, 'title': t.title, 'completed': t.completed} for t in tasks])
+
+@app.route('/tasks', methods=['POST'])
+def add_task():
+    data = request.json
+    new_task = Task(title=data['title'])
+    db.session.add(new_task)
+    db.session.commit()
+    return jsonify({'message': 'Task added successfully'})
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(host='0.0.0.0', port=5000, debug=True)
